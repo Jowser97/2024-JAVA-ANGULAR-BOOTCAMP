fd38118a82f11601046852ecc6971f80420c3ec6
Jowser97 <160051907+Jowser97@users.noreply.github.com>
2024-02-14 21:03:01 +0100

    Primeros ejercicios con Alex
    Me he descargado todo el material esencial y creado cuenta de github. He empezado a hacer los ejercicios de la tarea para saber como funciona eclipse.

---
+++ [ECLIPSE] Java Project - Tech Talent 2024/C1-intro_Java/src/TA2/ComentarioLinea.java
@@ -0,0 +1,16 @@
+package TA2;
+
+public class ComentarioLinea {
+
+	public static void main(String[] args) {
+		System.out.println("Hola");
+		/*He eliminado las barras del comentario
+		para ejecutar la linea que nos decia
+		hola en la consola*/
+		System.out.println("Adios");
+		System.out.println("FIN DE PROGRAMA");
+		
+
+	}
+
+}
