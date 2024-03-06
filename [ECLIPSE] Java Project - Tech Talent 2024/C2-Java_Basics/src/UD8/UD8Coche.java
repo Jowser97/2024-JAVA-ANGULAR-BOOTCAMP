d4b1717f4305be03faae1014369c1bdf3afe3bcb
Jowser97 <160051907+Jowser97@users.noreply.github.com>
2024-03-06 09:10:39 +0100

    Explicacion Objetos ud8

--- [ECLIPSE] Java Project - Tech Talent 2024/C2-Java_Basics/src/UD8/UD8Coche.java
+++ [ECLIPSE] Java Project - Tech Talent 2024/C2-Java_Basics/src/UD8/UD8Coche.java
@@ -1,5 +1,79 @@
 package UD8;
 
 public class UD8Coche {
-
+    // Atributos
+    private String marca;
+    private String modelo;
+    private int año;
+    private String color;
+    
+    // Constructor
+    public UD8Coche() {
+        this.marca = "";
+        this.modelo = "";
+        this.año = 0;
+        this.color = "";
+    }
+    
+    public UD8Coche(String marca, String modelo, int año, String color) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+        this.color = color;
+    }
+    
+    public UD8Coche(String marca, String modelo, int año, String color) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+        this.color = color;
+    }
+    
+    public UD8Coche(String marca, String modelo, int año, String color) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+        this.color = color;
+    }
+    
+    public UD8Coche(String marca, String modelo, int año, String color) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+        this.color = color;
+    }
+    
+    
+    public UD8Coche(String marca, String modelo, int año, String color) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+        this.color = color;
+    }
+    
+    // Métodos
+    public void arrancar() {
+        System.out.println("El coche " + marca + " " + modelo + " ha arrancado.");
+    }
+    
+    public void detener() {
+        System.out.println("El coche " + marca + " " + modelo + " se ha detenido.");
+    }
+    
+    public void acelerar() {
+        System.out.println("El coche " + marca + " " + modelo + " está acelerando.");
+    }
+    
+    public void frenar() {
+        System.out.println("El coche " + marca + " " + modelo + " está frenando.");
+    }
+    
+    // Método principal para probar la clase
+    public static void main(String[] args) {
+        UD8Coche miCoche = new UD8Coche("Toyota", "Corolla", 2020, "rojo");
+        miCoche.arrancar();
+        miCoche.acelerar();
+        miCoche.frenar();
+        miCoche.detener();
+    }
 }
