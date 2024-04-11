3697b80096998f0ddba6b4df1b7c31d5d800798b
Jowser97 <160051907+Jowser97@users.noreply.github.com>
2024-04-11 11:28:50 +0200

    sql

--- [ECLIPSE] Java Project - Tech Talent 2024/C3-BBDD_Heidi/UD14-Introduccion/consulta_heidi_intro.sql
+++ [ECLIPSE] Java Project - Tech Talent 2024/C3-BBDD_Heidi/UD14-Introduccion/consulta_heidi_intro.sql
@@ -1,2 +1,9 @@
+CREATE DATABASE prueba_techtalent;
+USE prueba_techtalent;
 CREATE TABLE gente (nombre VARCHAR(40), fecha DATE);
+CREATE TABLE usuarios 
+(id INT AUTO_INCREMENT, 
+nombre VARCHAR(100) NOT NULL, 
+email VARCHAR(100) NOT NULL, 
+PRIMARY KEY (id));
 CREATE TABLE ciudad1 (nombre CHAR(20)NOT NULL, poblacion INT NULL);
